198eb2024f8ba9d0994f7541080ecf5e3902bdb6
Muhammad Azka <azkabdg@gmail.com>
2021-08-31 01:27:20 +0700

    Changed the index restAPI title message

--- src/api/routes/indexRoutes.js
+++ src/api/routes/indexRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    title: 'Back-end API',
+    title: 'Cashier Back-end API',
   });
 });
 module.exports = router;
