cb0149c9afb6df8b99e67fb7eebfcfdf13bbabb2
Muhammad Azka <azkabdg@gmail.com>
2021-08-31 01:28:47 +0700

    Changed the routing

--- src/api/routes/itemRoutes.js
+++ src/api/routes/itemRoutes.js
@@ -1,10 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
 const items = require('../controllers/itemController');
 
-module.exports = (app) => {
-  app.post('/items', items.create);
-  app.get('/items', items.getAll);
-  app.get('/items/:itemName', items.getByName);
-  app.get('/items/:itemId', items.getById);
-  app.put('/items/:itemId', items.update);
-  app.delete('/items/:itemId', items.delete);
-};
+router.get('/', items.getAll);
+router.get('/:itemId', items.getById);
+
+module.exports = router;
